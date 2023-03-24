48bedcb1cb59d23db3bf9aa4d07208860dcfe357
puccije <jeremy.pucci@etu.univ-grenoble-alpes.fr>
2023-03-24 16:03:47 +0100

    merge

--- viewer.py
+++ viewer.py
@@ -13,21 +13,6 @@ from core import *
 
 
 def main():
-    """ create a window, add scene objects, then run rendering loop """
-    viewer = Viewer()
-
-    # load shaders
-    shader = Shader('./shaders/phong.vert', './shaders/phong.frag')
-    skyboxShader = Shader('./shaders/skybox.vert', './shaders/skybox.frag')
-
-    viewer.add(heightMapTerrain(shader, './ress/heightmap.png', light_dir=(1, 0, 0)))
-    viewer.add(SkyBoxTexture(skyboxShader, np.array(['./ress/skybox/xpos.png', './ress/skybox/xneg.png',
-               './ress/skybox/ypos.png', './ress/skybox/yneg.png', './ress/skybox/zpos.png', './ress/skybox/zneg.png'])))
-    shaderparticle = Shader("./shaders/color.vert", "./shaders/color.frag")
-    viewer.add(PointAnimation(shaderparticle, 0, 0, 0, num_particles=1, point_size=10.0, texture_path='./ress/lava.png'))
-
-    # start rendering loop
-    viewer.run()
 
 
 if __name__ == '__main__':
