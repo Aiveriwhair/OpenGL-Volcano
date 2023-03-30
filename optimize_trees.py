c362ca61f05cbe771da84785d6b6b931813e1afc
puccije <jeremy.pucci@etu.univ-grenoble-alpes.fr>
2023-03-30 13:55:51 +0200

    trees opti good

--- optimize_trees.py
+++ optimize_trees.py
@@ -8,23 +8,27 @@ from math import radians
 
 
 class PineTrees(Node):
-    def __init__(self, shader, position, trunk_texture_path, leave_texture_path):
+    def __init__(self, shader, position, trunk_texture_path, leave_texture_path, **params):
         super().__init__()
         leave_tex = Texture(leave_texture_path)
         trunk_tex = Texture(trunk_texture_path)
 
         nb_trees = len(position)
-        trunk = self.generate_tree(
-            nb_trees, position, shader, leave_tex, trunk_tex)
-        self.add(trunk)
+        trees = self.generate_tree(
+            nb_trees, position, shader, leave_tex, trunk_tex, **params)
+        self.add(trees)
 
-    def generate_tree(self, nb_trees, position, shader, leave_tex, trunk_tex):
-        trunks = Generate_Trunk(nb_trees, position, shader, trunk_tex)
-        return trunks
+    def generate_tree(self, nb_trees, position, shader, leave_tex, trunk_tex, **params):
+        trunks = Generate_Trunk(
+            nb_trees, position, shader, trunk_tex, **params)
+        leaves = Generate_Leaves(
+            nb_trees, position, shader, leave_tex, **params)
+        tree = Node([trunks, leaves])
+        return tree
 
 
 class Generate_Trunk(Textured):
-    def __init__(self, nb_trees, position, shader, trunk_tex, min_height=10, height=7, division=10, r=0.5, **params):
+    def __init__(self, nb_trees, position, shader, trunk_tex, min_height=10, height=7, division=6, r=0.5, **params):
         self.height = height
         self.division = division
         self.r = r
@@ -39,70 +43,128 @@ class Generate_Trunk(Textured):
             use_texture3=False,
         )
 
-        trunk_vert = self.division*2+2
-        trunk_vertices = self.generate_trunk_vertices(trunk_vert)
-        tex_coords_trunk = self.generate_trunk_tex_coords(trunk_vert)
-        indices_trunk = self.generate_trunk_indices()
+        vertices = self.generate_trunk_vertices(nb_trees, position)
+        tex_coords = self.generate_trunk_tex_coords(nb_trees)
+        indices = self.generate_trunk_indices(nb_trees)
 
-        vertices = np.empty(
-            shape=(nb_trees * trunk_vert, 3), dtype=np.float32)
-        indices = np.empty(
-            shape=(nb_trees * (6 * self.division), 3), dtype=np.uint32)
-        tex_coords = np.empty(
-            shape=(nb_trees * trunk_vert, 2), dtype=np.float32)
+        normals = calculate_normals2(vertices, indices)
 
-        for i in range(0, nb_trees):
-            start_vert = i * trunk_vert
-            start_ind = i * (6 * self.division)
-
-            vertices[start_vert: start_vert +
-                     trunk_vert] = trunk_vertices + position[i]
-            indices[start_ind: start_ind + (6 * self.division)
-                    ] = indices_trunk.reshape(-1, 3) + i * trunk_vert
-            tex_coords[start_vert: start_vert + trunk_vert] = tex_coords_trunk
-
-        # normals = calculate_normals3(vertices, indices)
         mesh = Mesh(shader, attributes=dict(position=vertices, texcoord=tex_coords,
-                                            ), index=indices, **{**uniforms, **params})
+                                            normal=normals,), index=indices, **{**uniforms, **params})
         super().__init__(mesh, texture_sampler=trunk_tex)
 
-    def generate_trunk_vertices(self, trunk_vert):
-        res = np.empty(shape=(trunk_vert, 3), dtype=np.float32)
+    def generate_trunk_vertices(self, nb_trees, position):
+        vertices = []
+        for i in range(nb_trees):
+            x, y, z = position[i]
+            for j in range(self.division + 1):
+                theta = 2 * np.pi * j / self.division
+                vertices.append([x + self.r * np.cos(theta),
+                                y, z + self.r * np.sin(theta)])
+                vertices.append([x + self.r * np.cos(theta),
+                                y + self.height, z + self.r * np.sin(theta)])
 
-        count = 0
-        for i in np.linspace(0, 2 * np.pi, self.division, endpoint=False):
-            res[count] = [self.r * np.cos(i), self.height, self.r * np.sin(i)]
-            count += 1
-            res[count] = [self.r * np.cos(i), 0, self.r * np.sin(i)]
-            count += 1
+            # Add top and bottom center vertices
+            vertices.append([x, y, z])
+            vertices.append([x, y + self.height, z])
 
-        return res
+        return np.array(vertices, dtype=np.float32)
 
-    def generate_trunk_tex_coords(self, trunk_vert):
-        res = np.empty(shape=(trunk_vert, 2), dtype=np.float32)
+    def generate_trunk_tex_coords(self, nb_trees):
+        tex_coords = []
+        for i in range(nb_trees):
+            for j in range(self.division + 1):
+                tex_coords.append([j / self.division, 0])
+                tex_coords.append([j / self.division, 1])
 
-        count = 0
-        for i in range(self.division):
-            res[count] = [i / self.division, 1]
-            count += 1
-            res[count] = [i / self.division, 0]
-            count += 1
+            # Add texture coordinates for top and bottom center vertices
+            tex_coords.append([0.5, 0])
+            tex_coords.append([0.5, 1])
 
-        return res
+        return np.array(tex_coords, dtype=np.float32)
 
-    def generate_trunk_indices(self):
-        res = np.empty(shape=(6 * self.division, 3), dtype=np.uint32)
+    def generate_trunk_indices(self, nb_trees):
+        indices = []
+        for i in range(nb_trees):
+            base = i * (2 * (self.division + 1) + 2)
+            top_center = base + 2 * (self.division + 1)
+            bottom_center = top_center + 1
 
-        count = 0
-        for i in range(0, self.division):
-            if i != self.division - 1:
-                res[count] = [2 * i, 2 * i + 1,
-                              (2 * i + 2) % (2 * self.division)]
-                res[count + 1] = [2 * i + 1, (2 * i + 3) % (2 * self.division),
-                                  (2 * i + 2) % (2 * self.division)]
-            else:
-                res[count] = [2 * i, 2 * i + 1, 0]
-                res[count + 1] = [2 * i + 1, 1, 0]
-            count += 2
+            for j in range(self.division):
+                indices.append([base + 2 * j, base + 2 * j + 1,
+                               base + 2 * ((j + 1) % (self.division + 1))])
+                indices.append([base + 2 * j + 1, base + 2 * ((j + 1) %
+                               (self.division + 1)) + 1, base + 2 * ((j + 1) % (self.division + 1))])
 
-        return res.flatten()
+                # Add indices for top and bottom faces
+                indices.append([top_center, base + 2 * j, base + 2 * (j + 1) %
+                               (2 * (self.division + 1))])  # Correct order for the top face
+                # Inverted order for the bottom face
+                indices.append([bottom_center, base + 2 * (j + 1) %
+                               (2 * (self.division + 1)) + 1, base + 2 * j + 1])
+
+        return np.array(indices, dtype=np.uint32)
+
+
+class Generate_Leaves(Textured):
+    def __init__(self, nb_trees, position, shader, leave_tex, heights=(8, 7, 6), widths=(4, 5, 7), subdivisions=15, **params):
+        self.heights = heights
+        self.widths = widths
+        self.subdivisions = subdivisions
+
+        uniforms = dict(
+            k_d=np.array((0.8, 0.8, 0.8), dtype=np.float32),
+            k_s=np.array((0.5673, 0.5673, 0.5673), dtype=np.float32),
+            k_a=np.array((0.5, 0.5, 0.5), dtype=np.float32),
+            s=60,
+            use_texture2=False,
+            use_texture3=False,
+        )
+
+        vertices = self.generate_leaves_vertices(nb_trees, position)
+        tex_coords = self.generate_leaves_tex_coords(nb_trees)
+        indices = self.generate_leaves_indices(nb_trees)
+
+        normals = calculate_normals2(vertices, indices)
+
+        mesh = Mesh(shader, attributes=dict(position=vertices,
+                    texcoord=tex_coords, normal=normals), index=indices, **{**uniforms, **params})
+        super().__init__(mesh, texture_sampler=leave_tex)
+
+    def generate_leaves_vertices(self, nb_trees, position):
+        vertices = []
+        for x, y, z in position:
+            for h, w in zip(self.heights, self.widths):
+                y_base = y + h - w / 2
+                tip = [x, y_base + w / 2, z]
+
+                base_vertices = []
+                for i in range(self.subdivisions):
+                    angle = 2 * np.pi * i / self.subdivisions
+                    base_vertices.append(
+                        [x + w / 2 * np.cos(angle), y_base, z + w / 2 * np.sin(angle)])
+
+                vertices.append(tip)
+                vertices.extend(base_vertices)
+
+        return np.array(vertices, dtype=np.float32)
+
+    def generate_leaves_tex_coords(self, nb_trees):
+        tex_coords = []
+        for i in range(nb_trees * len(self.heights)):
+            tex_coords.append([0.5, 1])
+            for j in range(self.subdivisions):
+                tex_coords.append([0.5 * (1 + np.cos(2 * np.pi * j / self.subdivisions)),
+                                   0.5 * (1 + np.sin(2 * np.pi * j / self.subdivisions))])
+
+        return np.array(tex_coords, dtype=np.float32)
+
+    def generate_leaves_indices(self, nb_trees):
+        indices = []
+        for i in range(nb_trees * len(self.heights)):
+            base = i * (self.subdivisions + 1)
+            for j in range(self.subdivisions):
+                indices.append([base, base + 1 + (j + 1) %
+                                self.subdivisions, base + 1 + j])
+
+        return np.array(indices, dtype=np.uint32)
--- viewer.py
+++ viewer.py
@@ -30,23 +30,26 @@ def main():
     #     viewer.add(forest[i])
 
     terrain = heightMapTerrain(
-        shaderTree, './ress/hm2.png', light_dir=(1, 0, 0), height_factor=0.6, numbertrees=1, red_tint_factor=0.)
-    pos = terrain.pos_trees
+        shaderTree, './ress/hm2.png', light_dir=(1, 0, 0), height_factor=0.6, numbertrees=200, red_tint_factor=0.)
+    pos_trees = terrain.pos_trees
+    # cut the first 15 pos_trees to have two different pos_trees arrays
+    pos_trees2_pine = pos_trees[0:]
+    pos_trees_oak = pos_trees[:0]
 
-    viewer.add(PineTrees(shaderTree, pos,
-               './ress/grass.png', './ress/grass.png'))
+    viewer.add(PineTrees(shaderTree, pos_trees2_pine,
+               './ress/wood.png', './ress/pine.jpg', light_dir=(1, 0, 0)))
 
-    # for i in range(len(pos)):
-    #     viewer.add(forestGenerator(shaderTree, 1,
-    #                pos[i][0], pos[i][1], pos[i][2], light_dir=(1, 0, 0))[0])
+    for i in range(len(pos_trees_oak)):
+        viewer.add(forestGenerator(shaderTree, 1,
+                                   pos_trees_oak[i][0], pos_trees_oak[i][1], pos_trees_oak[i][2], light_dir=(1, 0, 0))[0])
     viewer.add(terrain)
     viewer.add(SkyBoxTexture(skyboxShader, np.array(['./ress/skybox/xpos.png', './ress/skybox/xneg.png',
                './ress/skybox/ypos.png', './ress/skybox/yneg.png', './ress/skybox/zpos.png', './ress/skybox/zneg.png'])))
     # for i in range(len(pos)):
-    # viewer.add(PointAnimation(shaderTree,  pos[i][0], pos[i][1]+8, pos[i][2], './ress/grass.png', num_particles=15,
-    #                         point_size=10.0, light_dir=(1, 0, 0)))
-    viewer.add(*load('./drag1.obj', shaderTree,
-               light_dir=(1, 0, 0), K_d=(.6, .7, .8), s=100))
+    # viewer.add(PointAnimation(shaderTree,  pos_trees[i][0], pos_trees[i][1]+8, pos_trees[i][2], './ress/grass.png', num_particles=15,
+    #                           point_size=10.0, light_dir=(1, 0, 0)))
+    # viewer.add(*load('./drag1.obj', shaderTree,
+    #            light_dir=(1, 0, 0), K_d=(.6, .7, .8), s=100))
 
     # start rendering loop
     viewer.run()
